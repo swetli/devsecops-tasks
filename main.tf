bf8bfc924dff5cbe65b413e8f26d39ba688527ec
Svetli <swetlibg@gmail.com>
2024-07-12 10:46:50 +0300

    Add changes

---
+++ main.tf
@@ -0,0 +1,17 @@
+resource "terraform_data" "first" {
+  provisioner "local-exec" {
+    command = "echo 1"
+  }
+}
+
+resource "terraform_data" "second" {
+  provisioner "local-exec" {
+    command = "echo 2"
+  }
+}
+
+resource "terraform_data" "third" {
+  provisioner "local-exec" {
+    command = "echo 3"
+  }
+}
